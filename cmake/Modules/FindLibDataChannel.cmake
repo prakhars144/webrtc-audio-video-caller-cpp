eb15fbea878467749ea87d7545a8654d3d39c9e0
Akshay V <45287844+akshayvcr7@users.noreply.github.com>
2024-03-27 12:24:07 +0530

    add libdatachannel support (#2)

---
+++ cmake/Modules/FindLibDataChannel.cmake
@@ -0,0 +1,53 @@
+#[=======================================================================[.rst
+FindLibDataChannel
+----------
+
+FindModule for LibDataChannel library
+
+Imported Targets
+^^^^^^^^^^^^^^^^
+
+This module defines the :prop_tgt:`IMPORTED` target ``LibDataChannel::LibDataChannel``.
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This module sets the following variables:
+
+``LibDataChannel_FOUND``
+  True, if the library was found.
+
+Cache variables
+^^^^^^^^^^^^^^^
+
+The following cache variables may also be set:
+
+``LibDataChannel_INCLUDE_DIR``
+  Directory containing ``rtc/rtc.hpp``.
+
+#]=======================================================================]
+
+include(FindPackageHandleStandardArgs)
+
+find_path(
+  LibDataChannel_INCLUDE_DIR
+  NAMES rtc/rtc.hpp
+  PATHS ${PROJECT_SOURCE_DIR}/libdatachannel/include)
+
+find_package_handle_standard_args(
+  LibDataChannel
+  REQUIRED_VARS LibDataChannel_INCLUDE_DIR)
+mark_as_advanced(LibDataChannel_INCLUDE_DIR)
+
+if(LibDataChannel_FOUND)
+  if(NOT TARGET LibDataChannel::LibDataChannel)
+     if(IS_ABSOLUTE "${LibDataChannel_LIBRARY}")
+        add_library(LibDataChannel::LibDataChannel UNKNOWN IMPORTED)
+        set_property(TARGET LibDataChannel::LibDataChannel PROPERTY IMPORTED_LOCATION "${LibDataChannel_LIBRARY}")
+    else()
+        add_library(LibDataChannel::LibDataChannel INTERFACE IMPORTED)
+        set_property(TARGET LibDataChannel::LibDataChannel PROPERTY IMPORTED_LIBNAME "${LibDataChannel_LIBRARY}")
+    endif()
+    set_target_properties(LibDataChannel::LibDataChannel PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${LibDataChannel_INCLUDE_DIR}")
+  endif()
+endif()
